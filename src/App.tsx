33665e55df1644a7f1d751fb37c3218ff952d296
Gabriel Bursi <gabriellbursii@gmail.com>
2022-12-13 16:29:17 -0300

    configurando as rotas das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,57 @@
 import React from 'react';
-import Step1Page from './pages/Step1Page';
+import { Link, useNavigate } from 'react-router-dom';
+import RoutesSteps from './Routes';
 
 import './styles/App.scss'
 
 function App() {
+
+  const navigate = useNavigate()
+
   return (
     <div className='container'>
       <main className='main'>
         <aside className='aside'>
           <div className='cont-steps'>
             <div className='step'>
-              <div className='step-number'>
-                1
+              <div className='step-number' onClick={()=>navigate('/')}>
+                <Link to='/'>1</Link>
               </div>
               <div className='text-step'>
                 <span>STEP 1</span>
-                <p>YOUR INFO</p>
+                <Link to='/'><p>YOUR INFO</p></Link>
               </div>
             </div>
             <div className='step'>
-              <div className='step-number'>
-                2
+              <div className='step-number' onClick={()=>navigate('/step2')}>
+                <Link to='/step2'>2</Link>
               </div>
               <div className='text-step'>
                 <span>STEP 2</span>
-                <p>SELECT PLAN</p>
+                <Link to='/step2'><p>SELECT PLAN</p></Link>
               </div>
             </div>
             <div className='step'>
-              <div className='step-number'>
-                3
+              <div className='step-number' onClick={()=>navigate('/step3')}>
+                <Link to='/step3'>3</Link>
               </div>
               <div className='text-step'>
                 <span>STEP 3</span>
-                <p>ADD-ONS</p>
+                <Link to='/step3'><p>ADD-ONS</p></Link>
               </div>
             </div>
             <div className='step'>
-              <div className='step-number'>
-                4
+              <div className='step-number' onClick={()=>navigate('/step4')}>
+                <Link to='/step4'>4</Link>
               </div>
               <div className='text-step'>
                 <span>STEP 4</span>
-                <p>SUMMARY</p>
+                <Link to='/step4'><p>SUMMARY</p></Link>
               </div>
             </div>
           </div>
         </aside>
-        <Step1Page/>
+        <RoutesSteps/>
       </main>
     </div>
   );
