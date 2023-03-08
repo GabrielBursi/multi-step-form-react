5959dd55ce36d377d38aa510d576eae8286b62ec
Gabriel Bursi <gabriellbursii@gmail.com>
2023-03-08 18:23:44 -0300

    criando componente para sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,31 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
+import { SideBar } from './components';
 import { StepsContext } from './context';
 import RoutesSteps from './Routes';
 
 import './styles/App.scss';
 
+type Steps = {
+	stepActive: boolean,
+	stepPage: number,
+	title: string
+}
+
 function App() {
 
 	const { step1, step2, step3, step4 } = useContext(StepsContext);
 
+	const steps: Steps[] = [
+			{ stepActive: step1, stepPage: 1, title: 'YOUR INFO' },
+			{ stepActive: step2, stepPage: 2, title: 'SELECT PLAN' },
+			{ stepActive: step3, stepPage: 3, title: 'ADD-ONS' },
+			{ stepActive: step4, stepPage: 4, title: 'SUMMARY' },
+		]
+
 	return (
 		<div className='container'>
 			<main className='main'>
-				<aside className='aside'>
-					<div className='cont-steps'>
-						<div className='step'>
-							<div className={`${step1 ? 'hover-number' : 'step-number'}`}>
-								<p>1</p>
-							</div>
-							<div className='text-step'>
-								<p className='step-p'>STEP 1</p>
-								<p><p className='step-p2'>YOUR INFO</p></p>
-							</div>
-						</div>
-						<div className='step'>
-							<div className={`${step2 ? 'hover-number' : 'step-number'}`}>
-								<p>2</p>
-							</div>
-							<div className='text-step'>
-								<p className='step-p'>STEP 2</p>
-								<p><p className='step-p2'>SELECT PLAN</p></p>
-							</div>
-						</div>
-						<div className='step'>
-							<div className={`${step3 ? 'hover-number' : 'step-number'}`}>
-								<p>3</p>
-							</div>
-							<div className='text-step'>
-								<p className='step-p'>STEP 3</p>
-								<p><p className='step-p2'>ADD-ONS</p></p>
-							</div>
-						</div>
-						<div className='step'>
-							<div className={`${step4 ? 'hover-number' : 'step-number'}`}>
-								<p>4</p>
-							</div>
-							<div className='text-step'>
-								<p className='step-p'>STEP 4</p>
-								<p><p className='step-p2'>SUMMARY</p></p>
-							</div>
-						</div>
-					</div>
-				</aside>
+				<SideBar steps={steps}/>
 				<RoutesSteps/>
 			</main>
 		</div>
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface SideBarProps {
+    steps: Steps[]
+}
+
+type Steps = {
+    stepActive: boolean,
+    stepPage: number,
+    title: string
+}
+
+export function SideBar({ steps }: SideBarProps) {
+
+    return (
+        <aside className='aside'>
+            <div className='cont-steps'>
+                {steps.map(step => (
+                    <div className='step'>
+                        <div className={`${step.stepActive ? 'hover-number' : 'step-number'}`}>
+                            <p>{step.stepPage}</p>
+                        </div>
+                        <div className='text-step'>
+                            <p className='step-p'>STEP {step.stepPage}</p>
+                            <p><p className='step-p2'>{step.title}</p></p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </aside>
+    );
+}
